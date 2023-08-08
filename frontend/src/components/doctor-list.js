cd8d0b237b0c84cc829d55b1d9db45f752f012c6
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2023-08-08 17:56:02 -0400

    ldsfds

--- frontend/src/components/doctor-list.js
+++ frontend/src/components/doctor-list.js
@@ -453,9 +453,21 @@ const renderDoctorReviews = (doctor) => {
   );
 };
 
-  
-  // ... (previous code)
-  
+  const LoginPopup = () => {
+    return (
+      <div className="modal-backdrop">
+        <div className="modal-container">
+          <div className="modal-content">
+            <p>Please log in to add a review.</p>
+            {/* Link to localhost:8080/login */}
+            <a href="https://doctorlook.onrender.com/api/v1/doctors/login" className="btn btn-primary">Login</a>
+            <button className="btn btn-primary" onClick={togglePopup}>Close</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="container-fluid">
       
@@ -563,21 +575,7 @@ const renderDoctorReviews = (doctor) => {
                 </>
               ) : (
                 // Show a login popup when the user is not logged in and tries to add a review
-                <button className="btn btn-primary" onClick={() => { togglePopup(); LoginPopup = () => {
-                  return (
-                    <div className="modal-backdrop">
-                    <div className="modal-container">
-                      <div className="modal-content">
-                        <p>Please log in to add a review.</p>
-                        {/* Link to localhost:8080/login */}
-                        <a href="https://doctorlook.onrender.com/api/v1/doctors/login" className="btn btn-primary">Login</a>
-                        <button className="btn btn-primary" onClick={togglePopup()}>Close</button>
-                      </div>
-                    </div>
-                  </div>
-                  
-                  );
-                };}}>
+                <button className="btn btn-primary" onClick={ togglePopup() && <LoginPopup /> }>
                   Add Review (Log in required)
                 </button>
 
