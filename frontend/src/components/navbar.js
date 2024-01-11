02d3d1020e9426a9496e50612f3f2edeec5d000d
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2024-01-10 20:27:43 -0500

    sdf

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -110,7 +110,6 @@ export default function NavBar({ user, login, logout}) {
                           color: "black",
                           cursor: "pointer",
                           transition: "color 0.3s",
-                          borderBottom: hoveredButton === 'login' ? '2px solid #18A1CC' : 'none',
                         }}
                         onMouseEnter={(e) => e.target.style.color = "#18A1CC"}
                         onMouseLeave={(e) => e.target.style.color = "black"}
